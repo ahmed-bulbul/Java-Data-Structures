1800032458ba33e7e18f3f4368e9dc5d458e2588
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-11-25 15:21:36 +0600

    Done

--- src/main/java/com/algo/WorkingWithMap.java
+++ src/main/java/com/algo/WorkingWithMap.java
@@ -2,12 +2,23 @@ package com.algo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMap {
 
     public static void main(String[] args) {
 
 
+        Map<Person,Diamond> map = new HashMap<>();
+        map.put(new Person("A",1),new Diamond("African Diamond"));
+        System.out.println(new Person("A",1).hashCode());
+        System.out.println(new Person("A",1).hashCode());
+        System.out.println(map.get(new Person("A",1)));
+
+
+    }
+
+    public static void maps(){
         Map<Integer,Person> map = new HashMap<>();
         map.put(1,new Person("A",1));
         map.put(2,new Person("B",2));
@@ -27,9 +38,38 @@ public class WorkingWithMap {
         });
         System.out.println(map.getOrDefault(1,new Person("Default",1)));
         System.out.println(map.values());
-
-
     }
 
-    record Person(String name, int age) {}
+    static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return age == person.age && Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
+    }
+
+    record Diamond(String name) {}
 }
